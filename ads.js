71fe00cb1303605f2a65bcf4aa0b897a15db25cc
Gaurav <gauravkr2506@gmail.com>
2022-09-03 16:42:13 +0530

    demo1

--- ads.js
+++ ads.js
@@ -4,6 +4,7 @@ var adsLoaded = false;
 var adContainer;
 var adDisplayContainer;
 var adsLoader;
+var adsManager;
 
 // On window load, attach an event to the play button click
 // that triggers playback on the video element
@@ -21,19 +22,36 @@ window.addEventListener("load", function (event) {
 
 window.addEventListener("resize", function (event) {
   console.log("window resized");
+  if (adsManager) {
+    var width = videoElement.clientWidth;
+    var height = videoElement.clientHeight;
+    adsManager.resize(width, height, google.ima.ViewMode.NORMAL);
+  }
 });
 
 function initializeIMA() {
   console.log("initializing IMA");
-  adContainer = document.getElementById("ad-container");
+  adContainer = document.getElementById("ad-container"); //
+  adContainer.addEventListener("click", adContainerClick);
   adDisplayContainer = new google.ima.AdDisplayContainer(
     adContainer,
     videoElement
+  ); //
+  adsLoader = new google.ima.AdsLoader(adDisplayContainer); //
+  adsLoader.addEventListener(
+    google.ima.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED,
+    onAdsManagerLoaded,
+    false
   );
-  adsLoader = new google.ima.AdsLoader(adDisplayContainer);
+  adsLoader.addEventListener(
+    google.ima.AdErrorEvent.Type.AD_ERROR,
+    onAdError,
+    false
+  );
   // Let the AdsLoader know when the video has ended
   videoElement.addEventListener("ended", function () {
     adsLoader.contentComplete();
+    console.log("end ad");
   });
 
   var adsRequest = new google.ima.AdsRequest();
@@ -64,4 +82,68 @@ function loadAds(event) {
   event.preventDefault();
 
   console.log("loading ads");
+  // Initialize the container. Must be done via a user action on mobile devices.
+  videoElement.load();
+  adDisplayContainer.initialize();
+
+  var width = videoElement.clientWidth;
+  var height = videoElement.clientHeight;
+  try {
+    adsManager.init(width, height, google.ima.ViewMode.NORMAL);
+    adsManager.start();
+  } catch (adError) {
+    // Play the video without ads, if an error occurs
+    console.log("AdsManager could not be started");
+    videoElement.play();
+  }
+}
+
+function onAdsManagerLoaded(adsManagerLoadedEvent) {
+  // Instantiate the AdsManager from the adsLoader response and pass it the video element
+  adsManager = adsManagerLoadedEvent.getAdsManager(videoElement);
+  adsManager.addEventListener(google.ima.AdErrorEvent.Type.AD_ERROR, onAdError);
+  adsManager.addEventListener(
+    google.ima.AdEvent.Type.CONTENT_PAUSE_REQUESTED,
+    onContentPauseRequested
+  );
+  adsManager.addEventListener(
+    google.ima.AdEvent.Type.CONTENT_RESUME_REQUESTED,
+    onContentResumeRequested
+  );
+  adsManager.addEventListener(google.ima.AdEvent.Type.LOADED, onAdLoaded);
+}
+
+function onAdError(adErrorEvent) {
+  // Handle the error logging.
+  console.log(adErrorEvent.getError());
+  if (adsManager) {
+    adsManager.destroy();
+  }
+}
+
+function onContentPauseRequested() {
+  videoElement.pause();
+  console.log("pause");
+}
+
+function onContentResumeRequested() {
+  videoElement.play();
+  console.log("play");
+}
+
+function adContainerClick(event) {
+  console.log("ad container clicked");
+  debugger;
+  if (videoElement.paused) {
+    videoElement.play();
+  } else {
+    videoElement.pause();
+  }
+}
+
+function onAdLoaded(adEvent) {
+  var ad = adEvent.getAd();
+  if (!ad.isLinear()) {
+    videoElement.play();
+  }
 }
