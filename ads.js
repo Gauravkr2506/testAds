88609e9292c4584270680657a9919060e33687c4
Gaurav <gauravkr2506@gmail.com>
2022-09-03 12:18:17 +0530

    added url

--- ads.js
+++ ads.js
@@ -1,3 +1,4 @@
+//https://gauravkr2506.github.io/testAds/
 var videoElement;
 var adsLoaded = false;
 var adContainer;
@@ -41,7 +42,6 @@ function initializeIMA() {
     "iu=/21775744923/external/single_ad_samples&sz=640x480&" +
     "cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&" +
     "gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=";
-  debugger;
   // Specify the linear and nonlinear slot sizes. This helps the SDK to
   // select the correct creative if multiple are returned.
   adsRequest.linearAdSlotWidth = videoElement.clientWidth;
